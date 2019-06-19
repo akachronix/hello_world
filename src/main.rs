3d7b9cc5b9ba3442ec7b6800262909a51666e2dc
akachronix <bgowengaming1@gmail.com>
2019-06-19 06:04:46 -0400

    vector fun

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,15 @@
-use std::io;
-use std::io::Write;
-use std::cmp::Ordering;
-use rand::Rng;
+use std::vec::Vec;
 
 fn main() {
     
-    print!("Guess the number! ");
-    io::stdout().flush();
-
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line!");
-    
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
-    
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("Secret number: {}", secret_number);
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big"),
+    let mut vec = Vec::new();
+    for i in 0..10 {
+        vec.push(i);
     }
+
+    for x in &vec {
+        print!("{} ", x);
+    }
+
+    print!("\n");
 }
